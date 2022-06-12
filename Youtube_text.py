5439f9294b9d18bf503366eab022aef175c67b54
valterhre <50119287+valterhre@users.noreply.github.com>
2022-06-12 12:49:52 +0300

    Add files via upload

---
+++ Youtube_text.py
@@ -0,0 +1,14 @@
+from pytube import YouTube
+from bs4 import BeautifulSoup
+import xml
+url=input()
+vid=YouTube(url)
+
+soup=BeautifulSoup(vid.captions.get_by_language_code('en').xml_captions, 'xml')
+
+t0=soup.find_all('p')
+with open(f'{vid.title[0:10]}.txt', 'w+') as f:
+    for k in range( len(t0)):
+        t=round((int(t0[k]['t'])/ 6000),2)
+        text=t0[k].get_text()
+        f.writelines(f'\n{t}\n{text}')
