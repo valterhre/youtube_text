61f667bf05cbb4275f61f4cd5159155cb6bb160f
valterhre <50119287+valterhre@users.noreply.github.com>
2022-06-12 13:01:51 +0300

    Update Youtube_text.py

--- Youtube_text.py
+++ Youtube_text.py
@@ -9,6 +9,6 @@ soup=BeautifulSoup(vid.captions.get_by_language_code('en').xml_captions, 'xml')
 t0=soup.find_all('p')
 with open(f'{vid.title[0:10]}.txt', 'w+') as f:
     for k in range( len(t0)):
-        t=round((int(t0[k]['t'])/ 6000),2)
+        t=round((int(t0[k]['t'])/ 1000),2)
         text=t0[k].get_text()
         f.writelines(f'\n{t}\n{text}')
